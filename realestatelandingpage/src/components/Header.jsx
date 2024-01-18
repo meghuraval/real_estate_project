c63b902300aaaf32bb4b2bdec89284c20983314e
meghu <meghuraval1234@gmail.com>
2024-01-18 17:38:10 -0500

    header is complete

--- realestatelandingpage/src/components/Header.jsx
+++ realestatelandingpage/src/components/Header.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <section className="h-wrapper">
-      <div className="h-container">
-        <img src="" alt=""></img>
+      <div className="h-container paddings innerWidth flexCenter">
+        <img src="./logo.png" alt="logo" width={100} />
+        <div className="flexCenter h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Values</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button" href="">
+            Contact
+          </button>
+        </div>
       </div>
     </section>
   );
