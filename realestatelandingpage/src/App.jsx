49ff731f689571096a19a9aa341ea8e0a1ace629
meghu <meghuraval1234@gmail.com>
2024-01-18 13:26:50 -0500

    added all images into file

--- realestatelandingpage/src/App.jsx
+++ realestatelandingpage/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <p>jello</p>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ realestatelandingpage/src/components/Header.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container">
+        <img src="" alt=""></img>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
