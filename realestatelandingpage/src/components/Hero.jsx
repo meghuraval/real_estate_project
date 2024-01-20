f71cda68ae67d96be2cfc18ef6a73343a6bfb355
meghu <meghuraval1234@gmail.com>
2024-01-20 13:01:27 -0500

    done

--- realestatelandingpage/src/components/Hero.jsx
+++ realestatelandingpage/src/components/Hero.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -12,10 +13,14 @@ const Hero = () => {
       <div className="flexColStart hero-left">
         <div className="hero-title">
           <div className="orange-circle" />
-          <h1>
+          <motion.h1
+            initial={{ y: "2rem", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2, type: "spring" }}
+          >
             Discover <br />
             Most Suitable <br /> Property
-          </h1>
+          </motion.h1>
         </div>
         <div className="flexColStart hero-description">
           <span className="secondaryText">
@@ -62,10 +67,15 @@ const Hero = () => {
       </div>
 
       {/*right side*/}
-      <div className="">
-        <div className="image-container">
+      <div className="flexCenter hero-right">
+        <motion.div
+          initial={{ x: "7rem", opacity: "0" }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2, type: "spring" }}
+          className="image-container"
+        >
           <img src="./hero-image.png" alt="hero-image" />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
