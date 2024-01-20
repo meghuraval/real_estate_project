8e2448208bd8ecdffc7d84d9bf3a2811462342e0
meghu <meghuraval1234@gmail.com>
2024-01-19 22:59:32 -0500

    residencies completed successfully

--- realestatelandingpage/src/App.jsx
+++ realestatelandingpage/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Companies from "./components/Companies";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Resedencies from "./components/Resedencies";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Hero />
       </div>
       <Companies />
+      <Resedencies />
     </div>
   );
 }
